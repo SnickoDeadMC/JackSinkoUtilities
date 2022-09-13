154030860425c178a52df84ee10f4701c415effc
Jack Sinko <jack.sinko@hotmail.com>
2022-09-13 21:13:19 +0930

    Add vector3 extensions

---
+++ Utilities/Vector3Extensions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Extensions
+{
+    
+    /// <summary>
+    /// Multiplies each element in Vector3 v by the corresponding element of w.
+    /// </summary>
+    public static Vector3 Multiply(this Vector3 a, Vector3 b)
+    {
+        a.x *= b.x;
+        a.y *= b.y;
+        a.z *= b.z;
+
+        return a;
+    }
+
+    public static Vector3 ClampValues(this Vector3 a, float min, float max)
+    {
+        float x = Mathf.Clamp(a.x, min, max);
+        float y = Mathf.Clamp(a.y, min, max);
+        float z = Mathf.Clamp(a.z, min, max);
+
+        return new Vector3(x, y, z);
+    }
+    
+}
