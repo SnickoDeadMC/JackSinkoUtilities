eb694274b25d0edeb03afc3470720fbd3ad091ba
Jack Sinko <jack.sinko@hotmail.com>
2023-07-21 18:55:59 +0930

    Add lazy singleton initialisation at runtime to ensure there's always an instance

--- Utilities/Singleton.cs
+++ Utilities/Singleton.cs
@@ -8,33 +8,30 @@ using MyBox;
 /// <remarks>You can add [ExecuteInEditMode] if you need the instance in edit mode (eg. for edit mode tests).</remarks>
 public abstract class Singleton<T> : Updatable where T : MonoBehaviour
 {
-
+    
     private static T instance;
     public static T Instance
     {
         get
         {
-            if (!Application.isPlaying)
+            if (instance == null)
             {
-                if (instance == null)
-                    instance = FindObjectOfType<T>();
-                return instance;
+                instance = FindObjectOfType<T>();
+                Debug.LogWarning($"Lazilly accessing {instance.name} singleton, as it is not yet initialised.");
             }
+            
             return instance;
         }
     }
 
     [Foldout("Singleton"), InitializationField, SerializeField]
     protected bool dontDestroy = true;
-
+    
     protected override void Initialise()
     {
         base.Initialise();
-
-        if (!Application.isPlaying)
-            return;
         
-        if (Instance != null)
+        if (instance != null)
         {
             //instance already exists, so just remove this new one
             gameObject.SetActive(false); //set inactive so it doesn't trigger colliders etc.
