68e879167a05ffde24d5ff11b586f35a9fa77d1c
Jack Sinko <jack.sinko@hotmail.com>
2023-07-25 22:50:38 +0930

    Add logger

---
+++ Runtime/Logger.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[CreateAssetMenu(fileName = "Logger")]
+public class Logger : ScriptableObject
+{
+
+    [SerializeField] private bool isEnabled = true;
+
+    /// <summary>
+    /// Shortcut to check if the logger exists before logging the message.
+    /// </summary>
+    [Conditional("ENABLE_LOGS")]
+    public static void Log(Logger logger, string message)
+    {
+        if (logger == null) return;
+        logger.Log(message);
+    }
+
+    [Conditional("ENABLE_LOGS")]
+    public void Log(string message)
+    {
+        if (!isEnabled) return;
+        try
+        {
+            Debug.Log("[" + name + "] " + message);
+        }
+        catch (Exception)
+        {
+            Debug.LogWarning("Error while logging.");
+        }
+    }
+    
+}
