c9d521b273b30c7f8a62c7a736d27d8dec37f986
Jack Sinko <jack.sinko@hotmail.com>
2023-08-04 14:31:01 +0930

    Add component utils

---
+++ Runtime/ComponentUtils.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ComponentUtils
+{
+    public static T GetComponent<T>(this GameObject gameObject, bool addIfNull) where T : Component
+    {
+        var value = gameObject.GetComponent<T>();
+        if (value == null && addIfNull)
+            value = gameObject.AddComponent<T>();
+        return value;
+    }
+ 
+    public static T GetComponent<T>(this Component component, bool addIfNull) where T : Component
+    {
+        return component.gameObject.GetComponent<T>(addIfNull);
+    }
+}
