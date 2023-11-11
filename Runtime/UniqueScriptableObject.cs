b31866794237896b0255d0f249d885fc01f45a25
Jack Sinko <jack.sinko@hotmail.com>
2023-11-11 11:58:14 +1030

    Add UniqueScriptableObject

---
+++ Runtime/UniqueScriptableObject.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Neko
+{
+    public class UniqueScriptableObject : ScriptableObject
+    {
+
+        [SerializeField, ReadOnly] private string uniqueID;
+        [SerializeField, HideInInspector] private string lastKnownName;
+
+        public string ID => uniqueID;
+        
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (string.IsNullOrEmpty(lastKnownName)
+                || !lastKnownName.Equals(name)
+                || string.IsNullOrEmpty(uniqueID))
+            {
+                GenerateNewID();
+            }
+        }
+
+        private void GenerateNewID()
+        {
+            lastKnownName = name;
+            uniqueID = GUID.Generate().ToString();
+            EditorUtility.SetDirty(this);
+        }
+#endif
+        
+    }
+}
