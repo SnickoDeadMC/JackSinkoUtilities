7c3999251493b0409d639121d9dcbdb134855e9a
Jack Sinko <jack.sinko@hotmail.com>
2023-11-29 00:20:42 +1030

    Add MainThreadDispatcher

---
+++ Runtime/MainThreadDispatcher.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace JacksUtils
+{
+    public class MainThreadDispatcher : Singleton<MainThreadDispatcher>
+    {
+        
+        private static readonly Queue<Action> _executionQueue = new();
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void InitialiseRuntime()
+        {
+            lock (_executionQueue)
+            {
+                _executionQueue.Clear();
+            }
+        }
+        
+        public void Update() {
+            lock(_executionQueue) {
+                while (_executionQueue.Count > 0) {
+                    _executionQueue.Dequeue().Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Locks the queue and adds the IEnumerator to the queue
+        /// </summary>
+        /// <param name="action">IEnumerator function that will be executed from the main thread.</param>
+        public void Enqueue(IEnumerator action) {
+            lock (_executionQueue) {
+                _executionQueue.Enqueue (() => {
+                    StartCoroutine (action);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Locks the queue and adds the Action to the queue
+        /// </summary>
+        /// <param name="action">function that will be executed from the main thread.</param>
+        public void Enqueue(Action action)
+        {
+            Enqueue(ActionWrapper(action));
+        }
+	
+        /// <summary>
+        /// Locks the queue and adds the Action to the queue, returning a Task which is completed when the action completes
+        /// </summary>
+        /// <param name="action">function that will be executed from the main thread.</param>
+        /// <returns>A Task that can be awaited until the action completes</returns>
+        public Task EnqueueAsync(Action action)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            void WrappedAction() {
+                try 
+                {
+                    action();
+                    tcs.TrySetResult(true);
+                } catch (Exception ex) 
+                {
+                    tcs.TrySetException(ex);
+                }
+            }
+
+            Enqueue(ActionWrapper(WrappedAction));
+            return tcs.Task;
+        }
+
+        IEnumerator ActionWrapper(Action a)
+        {
+            a();
+            yield return null;
+        }
+    }
+}
