a4b020188d0e6867e1a3a323343d69cf1fd54b77
Jack Sinko <jack.sinko@hotmail.com>
2024-01-12 10:35:42 +1030

    Add TODO to check if on the main thread for loggers

--- Runtime/Logger.cs
+++ Runtime/Logger.cs
@@ -12,28 +12,33 @@ namespace JacksUtils
     {
 
         [SerializeField] private bool isEnabled = true;
-
+        
         /// <summary>
         /// Shortcut to check if the logger exists before logging the message.
         /// </summary>
         [Conditional("ENABLE_LOGS")]
         public static void Log(Logger logger, string message)
         {
-            if (logger == null) return;
+            if (logger == null)
+                return;
+
             logger.Log(message);
         }
 
         [Conditional("ENABLE_LOGS")]
         public void Log(string message)
         {
-            if (!isEnabled) return;
+            if (!isEnabled)
+                return;
+
             try
             {
-                Debug.Log("[" + name + "] " + message);
+                //TODO: if it's an async thread, don't use the name - just use the message with [async]
+                Debug.Log($"[{name}] {message}");
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                Debug.LogWarning("Error while logging.");
+                Debug.LogWarning($"Error while logging.\n{e.Message}\n{e.StackTrace}");
             }
         }
 
