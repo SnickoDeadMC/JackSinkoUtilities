9fc458cd196dadd2b74c0ec1cbd9e0a5340bf898
Jack Sinko <jack.sinko@hotmail.com>
2024-01-12 16:31:05 +1030

    Add TransformUtils

---
+++ Runtime/TransformUtils.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacksUtils
+{
+    public static class TransformUtils
+    {
+
+        public static List<T> GetComponentsInAllChildren<T>(this Transform transform, List<T> existingComponents = null)
+        {
+            existingComponents ??= new List<T>();
+            
+            foreach (Transform child in transform)
+            {
+                T[] newComponents = child.GetComponents<T>();
+    
+                foreach (T component in newComponents)
+                {
+                    if (component != null)
+                        existingComponents.Add(component);
+                }
+    
+                GetComponentsInAllChildren<T>(child, existingComponents);
+            }
+    
+            return existingComponents;
+        }
+
+        public static T FindComponentInParents<T>(this Transform transform) where T : MonoBehaviour
+        {
+            Transform parent = transform;
+            while (parent != null)
+            {
+                T componentOnParent = parent.GetComponent<T>();
+                if (componentOnParent != null)
+                    return componentOnParent;
+
+                parent = parent.parent;
+            }
+
+            return null; //no component
+        }
+
+    }
+}
