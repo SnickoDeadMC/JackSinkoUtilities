6f8c5a4cf3f1317f6c473955c4326aa6524015fa
Jack Sinko <jack.sinko@hotmail.com>
2024-01-24 20:46:44 +1030

    Find panels whenever scene changes

--- Runtime/AnimatedPanels/PanelManager.cs
+++ Runtime/AnimatedPanels/PanelManager.cs
@@ -6,16 +6,12 @@ using System.Diagnostics;
 using MyBox;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Debug = UnityEngine.Debug;
 
 namespace JacksUtils
 {
     public class PanelManager : Singleton<PanelManager>
     {
-
-        [Tooltip("Panels that aren't under this object, that should be a part of the panel lookup.")]
-        [SerializeField] private AnimatedPanel[] additionalPanels;
-
+        
         [SerializeField, ReadOnly] private List<AnimatedPanel> panelStack = new();
 
         private readonly Dictionary<Type, AnimatedPanel> panelLookup = new();
@@ -26,9 +22,17 @@ namespace JacksUtils
         {
             base.Initialise();
 
-            CreatePanelLookup();
+            panelLookup.Clear();
+        
+            SceneManager.activeSceneChanged -= OnSceneChange;
+            SceneManager.activeSceneChanged += OnSceneChange;
         }
 
+        private void OnSceneChange(Scene oldScene, Scene newScene)
+        {
+            CreatePanelLookup();
+        }
+        
         public static T GetPanel<T>() where T : AnimatedPanel
         {
             return GetPanel(typeof(T)) as T;
@@ -38,14 +42,24 @@ namespace JacksUtils
         {
             if (!Instance.panelLookup.ContainsKey(panelType))
                 throw new NullReferenceException($"Could not find panel {panelType} in lookup. Scene is: {SceneManager.GetActiveScene().name}");
-
+        
             return Instance.panelLookup[panelType];
         }
-
+        
+        public static bool PanelExists<T>() where T : AnimatedPanel
+        {
+            return PanelExists(typeof(T));
+        }
+    
+        public static bool PanelExists(Type panelType)
+        {
+            return Instance.panelLookup.ContainsKey(panelType);
+        }
+        
         public void AddToStack(AnimatedPanel animatedPanel)
         {
             panelStack.Add(animatedPanel);
-
+        
             animatedPanel.OnAddToStack();
             UtilsLoggers.PanelLogger.Log($"Added {animatedPanel.gameObject.name} to stack.");
         }
@@ -55,41 +69,43 @@ namespace JacksUtils
             //show the previous panel if this was the last panel
             if (panelStack.Count >= 2 && panelStack[^1] == animatedPanel)
                 panelStack[^2].Show();
-
+        
             panelStack.Remove(animatedPanel);
 
             animatedPanel.OnRemoveFromStack();
             UtilsLoggers.PanelLogger.Log($"Removed {animatedPanel.gameObject.name} from stack.");
         }
 
+        /// <summary>
+        /// Clear the panel lookup and find and add all the panels in the current scene.
+        /// </summary>
         private void CreatePanelLookup()
         {
-            AnimatedPanel[] panels = GetComponentsInChildren<AnimatedPanel>(true);
-            foreach (AnimatedPanel panel in panels)
-            {
-                TryAddPanelToLookup(panel);
-            }
+            string sceneName = SceneManager.GetActiveScene().name;
+        
+            Stopwatch stopwatch = Stopwatch.StartNew();
 
-            foreach (AnimatedPanel panel in additionalPanels)
+            //remove null panels (if scene was unloaded etc.)
+            List<Type> keys = new List<Type>(panelLookup.Keys);
+            foreach (Type key in keys)
             {
-                TryAddPanelToLookup(panel);
+                if (panelLookup[key] == null)
+                {
+                    UtilsLoggers.PanelLogger.Log($"Removing {key} from panel lookup");
+                    panelLookup.Remove(key);
+                }
             }
+        
+            foreach (AnimatedPanel panel in SceneUtils.GetAllComponentsInActiveScene<AnimatedPanel>(true))
+            {
+                UtilsLoggers.PanelLogger.Log($"Adding {panel.GetType()} to panel lookup");
+                panelLookup[panel.GetType()] = panel;
+                if (panel.gameObject.activeInHierarchy)
+                    panel.Show(); //starts showing
+            }
+        
+            stopwatch.Stop();
+            UtilsLoggers.PanelLogger.Log($"Took {stopwatch.Elapsed.ToPrettyString(true)} to create the panel lookup for {sceneName}.");
         }
-
-        private void TryAddPanelToLookup(AnimatedPanel panel)
-        {
-            var panelType = panel.GetType();
-            if (panelLookup.ContainsKey(panelType))
-            {
-                Debug.LogError($"Multiple panels detected of type: {panelType.Name}");
-                return;
-            }
-
-            panel.gameObject.SetActive(false);
-            panelLookup.Add(panelType, panel);
-
-            UtilsLoggers.PanelLogger.Log($"Added {panel.GetType()} to panel lookup");
-        }
-
     }
 }
